9e65cc52af0b52cbed7ee6a7f758b7db7db847b7
Jeff W <jeffrey.weinberg89@gmail.com>
2019-01-19 18:51:18 -0700

    added navbar with functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,11 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.where(start: params[:start]..params[:end])
   end
 
   # GET /events/1
