708832ecd3fe7b57065e36d6bac39e452fc415aa
Jeff W <jeffrey.weinberg89@gmail.com>
2019-01-22 14:20:55 -0700

    comments section added to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
     has_many :todos
+    has_many :comments
     validates_presence_of :title
     validates_uniqueness_of :title
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
         patch :complete
       end
     end
+    resources :comments do
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
