5bb7ef4b3df2524a1f49c0a59f04ed2bc0b7d75c
Jeff W <jeffrey.weinberg89@gmail.com>
2019-01-22 15:36:53 -0700

    added delete function to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,22 @@ class CommentsController < ApplicationController
         @event = Event.find(params[:event_id])
         @comment = @event.comments.create(comment_params)
         redirect_to event_path(@event)
-      end
-     
-      private
-        def comment_params
-          params.require(:comment).permit(:author, :body)
+    end
+
+    def destroy
+        @event = Event.find(params[:event_id])
+        @comment = @event.comments.find(params[:id])
+        
+        if @comment.destroy
+         flash[:success] = "Todo List item was deleted."
+        else
+         flash[:error] = "Todo List item could not be deleted."
         end
+        redirect_to @event 
+    end
+     
+    private
+    def comment_params
+        params.require(:comment).permit(:author, :body)
+    end
 end
